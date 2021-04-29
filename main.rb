1e282173623912c4bd51b55a4960c8f2a768d2c1
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-04-29 21:20:45 +0800

    finish working with string

--- main.rb
+++ main.rb
@@ -11,10 +11,21 @@ age = 25
 age2 = -25
 gpa = 3.235434
 gpa2 = -3.235434
-ismale = true
 flaws = nil
 
 # working with string
+my_name = 'Muhammad Ariff Bin Norhadi'
+puts "#{my_name} is learning Ruby\nand put quotation here \" "
+puts my_name.upcase
+puts my_name.downcase
+puts my_name.length
+puts my_name.include? 'Ariff'
+puts my_name.include? 'Zakaria'
+puts my_name[0]
+puts my_name[7]
+puts my_name[0, 8]
+puts my_name.index('N')
+puts 'ariffnorhadi'.upcase
 
 
 
