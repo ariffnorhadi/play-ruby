33f9110eec8fac7f44ec49bd50652a79b9b6d5d2
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-04-29 21:08:58 +0800

    working with string

---
+++ main.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+character_name = 'Ariff'
+character_age = '35'
+
+puts "#{character_name} is #{character_age} years old"
+
+# Data types
+name = 'Arif'
+age = 25
+age2 = -25
+gpa = 3.235434
+gpa2 = -3.235434
+ismale = true
+flaws = nil
+
+# working with string
+
+
+
