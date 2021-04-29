74c8ec58016a21bcb8698db686a24dd7396d512b
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-04-29 22:03:36 +0800

    finish array

--- main.rb
+++ main.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
-# building a calculator
+# arrays
 
-puts 'Enter a number: '
-num1 = gets.chomp.to_f
-puts 'Enter another number: '
-num2 = gets.chomp
+friends = Array[1, 'Kevin', 'Karen', 'Ariff']
+puts friends
+puts friends[1]
+puts friends[-1]
+friends[0] = 'Fatimah'
+puts friends[0, 3]
 
-puts(num1 + num2.to_f)
+first_list = []
+first_list[0] = 'Norhadi'
+first_list[3] = 'Morsed'
+
+puts "#{first_list.length} is friend's size while #{first_list.length} is first list size"
+
+puts first_list.include? 'Norhadi'
+puts friends.sort
