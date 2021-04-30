74787efeca4a1a9015f1bcc20e141b7d78f3624d
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 07:43:49 +0800

    finish if else statements

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
-# return statement
+# if statements
 
-def cube(num)
-  [num * num * num, 70]
+is_man = true
+is_tall = false
+is_hungry = true
+
+if is_man && is_tall
+  puts 'I am a man and I am tall'
+elsif is_man && !is_tall
+  puts 'I am a man and I am short'
+else
+  puts 'I am not a main'
 end
 
-puts cube(2)[0]
-
-# return, same like java.
+if is_man && is_hungry
+  puts 'i am a man and i am hungry'
+elsif is_man && !is_hungry
+  puts 'i am a man and i am not hungry'
+else
+  puts 'i am not a man and i do not know what i feel'
+end
