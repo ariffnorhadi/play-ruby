a454db8166eb150350ce0ad7a4f7222ccd62edd2
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 09:51:54 +0800

    create simple quiz by using each loop, if statements, class and objects and print the results

--- main.rb
+++ main.rb
@@ -1,26 +1,39 @@
 # frozen_string_literal: true
 
-# classes and objects
+# quizzes
 
-class Book
-  attr_accessor :title, :author, :pages
+q1 = "What color is apple?\n(a)Red\n(b)Yellow"
+q2 = "What color is banana?\n(a)Red\n(b)Yellow"
+q3 = "What color is pear?\n(a)Green\n(b)Yellow"
 
-  def initialize(title, author, pages)
-    @author = author
-    @title = title
-    @pages = pages
+class Question
+  attr_accessor :question, :answer
+
+  def initialize(question, answer)
+    @question = question
+    @answer = answer
   end
-
-  def is_thick
-    if @pages >= 30
-      puts 'This book is thick'
-    else
-      puts 'This book is thin'
-    end
-  end
-
 end
 
-book1 = Book.new('Learning Ruby', 'Ariff Norhadi', 33)
-puts book1.author
-puts book1.is_thick
+question1 = Question.new(q1, 'a')
+question2 = Question.new(q2, 'b')
+question3 = Question.new(q3, 'a')
+
+questions = [question1, question2, question3]
+
+def run_quiz(questions)
+  answer = ''
+  score = 0
+  questions.each do |question|
+    puts question.question
+    answer = gets.chomp
+    score += 1 if answer == question.answer
+  end
+  if score.zero?
+    puts "Don't give up. Life has so much more to offer"
+  else
+    puts "Congratulations! You score #{score}/#{questions.length}"
+  end
+end
+
+run_quiz(questions)
