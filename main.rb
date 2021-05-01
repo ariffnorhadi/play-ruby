3a96a24dd492713242c256f854c7dc892be32703
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 10:02:19 +0800

    create simple inheritance by using chef and italian chef to use parent methods and override some of them

--- main.rb
+++ main.rb
@@ -1,39 +1,31 @@
 # frozen_string_literal: true
 
-# quizzes
+# inheritance
 
-q1 = "What color is apple?\n(a)Red\n(b)Yellow"
-q2 = "What color is banana?\n(a)Red\n(b)Yellow"
-q3 = "What color is pear?\n(a)Green\n(b)Yellow"
+class Chef
+  def make_chicken
+    puts 'The chef makes chicken'
+  end
 
-class Question
-  attr_accessor :question, :answer
-
-  def initialize(question, answer)
-    @question = question
-    @answer = answer
+  def make_special_dish
+    puts 'The chef makes BBQ'
   end
 end
 
-question1 = Question.new(q1, 'a')
-question2 = Question.new(q2, 'b')
-question3 = Question.new(q3, 'a')
+# < is used for inheritance
+class ItalianChef < Chef
 
-questions = [question1, question2, question3]
+  def make_special_dish
+    puts 'The Italian chef makes spaghetti and pasta'
+  end
 
-def run_quiz(questions)
-  answer = ''
-  score = 0
-  questions.each do |question|
-    puts question.question
-    answer = gets.chomp
-    score += 1 if answer == question.answer
-  end
-  if score.zero?
-    puts "Don't give up. Life has so much more to offer"
-  else
-    puts "Congratulations! You score #{score}/#{questions.length}"
-  end
 end
 
-run_quiz(questions)
+chef = Chef.new
+chef.make_special_dish
+
+italian_chef = ItalianChef.new
+italian_chef.make_chicken
+italian_chef.make_special_dish
+
+
