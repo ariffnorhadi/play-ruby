c8f8b6acbde14a64d707ae8c349c99483b2f69b9
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 08:03:50 +0800

    simple while loop

--- main.rb
+++ main.rb
@@ -1,27 +1,10 @@
 # frozen_string_literal: true
 
-# better calculator
+# loop
 
-def calculate(num1, operation, num2)
-  case operation
-  when '+'
-    puts num1 + num2
-  when '-'
-    puts num1 - num2
-  when '*'
-    puts num1 * num2
-  when '/'
-    puts num1 / num2
-  else
-    puts 'Invalid operator'
-  end
+count = 1
+while count < 5
+  puts count
+  count += 1
 end
 
-puts 'Enter first number: '
-num1 = gets.chomp.to_f
-puts 'Enter the operation:'
-operation = gets.chomp.to_s
-puts 'Enter the second number:'
-num2 = gets.chomp.to_f
-
-calculate(num1, operation, num2)
