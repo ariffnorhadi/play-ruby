9c6c72fb90aa79c001fabf7ab55437b3611aeae0
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 08:20:05 +0800

    create a list of friends and use for loop to print their names

--- main.rb
+++ main.rb
@@ -1,25 +1,8 @@
 # frozen_string_literal: true
 
-# secret game
+# for loop
 
-secret_word = 'Ruby'
-guess = ''
-guess_count = 1
-guess_limit = 3
-
-while secret_word != guess && guess_count <= guess_limit
-  puts "Atempt #{guess_count}"
-  puts 'Enter your guess:'
-  # always use chomp so enter value will not be counted as input
-  guess = gets.chomp
-  if guess == secret_word
-    puts "You have won after #{guess_count} attempt (s). Congratulations"
-    break
-  end
-  guess_count += 1
-  if guess_count > guess_limit
-    puts "You have failed. The secret word is #{secret_word}"
-    break
-  end
+friends = %w[Muhammad Ariff Aizat Aliff Mark]
+for friend in friends
+  puts friend
 end
-
