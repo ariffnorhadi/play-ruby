5b2154ed6f8820e76824039009ad878f86ab124e
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 08:23:31 +0800

    add more loop for numbers

--- main.rb
+++ main.rb
@@ -6,3 +6,12 @@ friends = %w[Muhammad Ariff Aizat Aliff Mark]
 friends.each do |friend|
   puts friend
 end
+
+# numbers
+(0..5).each do |index|
+  puts index
+end
+puts ''
+6.times do |index|
+  puts index
+end
