2bb8f380999c8437776187dd8898e053d3ea03b8
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 09:27:26 +0800

    create method in Book class to identify whether the book is thick or thin

--- main.rb
+++ main.rb
@@ -10,7 +10,17 @@ class Book
     @title = title
     @pages = pages
   end
+
+  def is_thick
+    if @pages >= 30
+      puts 'This book is thick'
+    else
+      puts 'This book is thin'
+    end
+  end
+
 end
 
 book1 = Book.new('Learning Ruby', 'Ariff Norhadi', 33)
 puts book1.author
+puts book1.is_thick
