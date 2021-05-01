007f24e6070e1871959d94d1c8a23b422c248bce
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-05-01 10:07:36 +0800

    create simple Tool module

--- main.rb
+++ main.rb
@@ -1,31 +1,7 @@
 # frozen_string_literal: true
 
-# inheritance
+# module
 
-class Chef
-  def make_chicken
-    puts 'The chef makes chicken'
-  end
-
-  def make_special_dish
-    puts 'The chef makes BBQ'
-  end
-end
-
-# < is used for inheritance
-class ItalianChef < Chef
-
-  def make_special_dish
-    puts 'The Italian chef makes spaghetti and pasta'
-  end
-
-end
-
-chef = Chef.new
-chef.make_special_dish
-
-italian_chef = ItalianChef.new
-italian_chef.make_chicken
-italian_chef.make_special_dish
-
-
+require_relative 'tools.rb'
+include Tools
+Tools.say_hi('Ariff Norhadi')
---
+++ tools.rb
@@ -0,0 +1,8 @@
+module Tools
+  def say_hi(name)
+    puts "hi #{name}"
+  end
+end
+
+# include Tools
+# Tools.say_hi('Ariff Norhadi')
