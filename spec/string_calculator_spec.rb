b1e0cc1264b77fe9854665a9d5eea225da26dcc4
Ariff Norhadi <ariff.norhadi@myfave.com>
2021-08-24 10:25:32 +0800

    setup rspec

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,7 @@
+# spec/string_calculator_spec.rb
+# References: https://semaphoreci.com/community/tutorials/getting-started-with-rspec
+
+require "string_calculator"
+
+describe StringCalculator do
+end
